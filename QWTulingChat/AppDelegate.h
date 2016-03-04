e23886eb2954cf81c4dbbd6ff65668632067c09c
sq-ios25 <qianwei_1018@163.com>
2016-03-04 19:31:03 +0800

    Initial Commit

---
+++ QWTulingChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QWTulingChat
+//
+//  Created by sq-ios25 on 16/3/4.
+//  Copyright © 2016年 sq-ios25. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QWTulingChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QWTulingChat
+//
+//  Created by sq-ios25 on 16/3/4.
+//  Copyright © 2016年 sq-ios25. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
